cfddeae04b573eaec6533e5c4db59ac569bbebcd
liberatojovanny44-ant <liberatojovanny44@gmail.com>
2026-04-01 20:20:20 -0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,16 @@
-const CACHE = 'lovely-v2';
-const BASE = '/lovely-pijamas';
+const CACHE_NAME = 'lovely-pijamas-v1';
 const ASSETS = [
-  BASE + '/',
-  BASE + '/index.html',
-  BASE + '/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match(BASE + '/index.html')))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
